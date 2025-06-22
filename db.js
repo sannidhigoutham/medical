1f3ed52d1c5da1376167b0bc6fa6176e394fda58
sannidhigoutham <42294507+sannidhigoutham@users.noreply.github.com>
2025-06-22 03:13:24 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host:"database-1.cn244wiagm8f.us-east-2.rds.amazonaws.com",
-  user:"admin",
-  password:"askfeteworld",
-  database:"Medical",
-  port:3308,
-});
+// const db = mysql.createConnection({
+//   host:"database-1.cn244wiagm8f.us-east-2.rds.amazonaws.com",
+//   user:"admin",
+//   password:"askfeteworld",
+//   database:"Medical",
+//   port:3308,
+// });
 
-db.connect((err)=>{
-  if(err){
-    throw err;
-  }
-});
+// db.connect((err)=>{
+//   if(err){
+//     throw err;
+//   }
+// });
 
-module.exports =db;
+// module.exports =db;
